bd0c69bcb38709c7d2c7b538fb6c8c8629c2bd56
Kyle Soskin <kyle.soskin@galileosuite.com>
2018-12-04 11:37:27 -0500

    Trying to make faster

--- day4/aocday4.rb
+++ day4/aocday4.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 
 require 'pp'
-require 'date'
+require 'time'
+require 'benchmark'
 
-@input = File.read('inputday4.log').lines.sort
+puts Benchmark.measure {
+@input = File.readlines('inputday4.log').sort
 
 def go_through_log(log, hash={})
-  log.each_with_index do |line, index|
+  log.each do |line|
     ts, info = line.split('] ')
     ts = ts.gsub('[','').gsub(' ', 'T')
     case info
@@ -16,12 +18,12 @@ def go_through_log(log, hash={})
     when /^falls/ 
       @start_time = ts
     when /^wakes/
-      startt = DateTime.parse(@start_time).to_time
-      endt = DateTime.parse(ts).to_time
-      delta = (endt - startt).to_i
+      startt = Time.parse(@start_time)
+      endt = Time.parse(ts)
+      delta = (endt.to_i - startt.to_i)
       mins_between = []
       while startt < endt
-       mins_between << startt.strftime('%M')
+       mins_between << startt.min
        startt += 60
       end
       hash[@current_guard] << {delta: delta, mins_between: mins_between}
@@ -32,10 +34,9 @@ end
 
 def part_1
   parsed = go_through_log(@input)
-  parsed = parsed.reject {|k,v| v.empty?}
   guard_with_most_sleep = parsed.max_by {|k,v| v.map {|h| h[:delta]}.inject(0) {|sum,x| sum + x }}
   all_mins = guard_with_most_sleep[1].map {|h| h[:mins_between]}.flatten
-  most_occuring_min = all_mins.max_by { |i| all_mins.count(i) }.to_i
+  most_occuring_min = all_mins.group_by(&:itself).values.max_by(&:size).first.to_i
   guard_num = guard_with_most_sleep[0].split('#').last.to_i
   pp [guard_num, most_occuring_min, guard_num * most_occuring_min]
 end
@@ -64,3 +65,4 @@ def part_2
 end
 
 ARGV[0] == ?a  ? part_1 : part_2
+}
