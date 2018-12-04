94b2e1c3ba78466d422909e4bf92e2b79cd3051e
Kyle Soskin <kyle.soskin@galileosuite.com>
2018-12-04 09:56:06 -0500

    Answer

--- day4/aocday4.rb
+++ day4/aocday4.rb
@@ -3,7 +3,7 @@
 require 'pp'
 require 'date'
 
-input = File.read('inputday4.log').lines.sort
+@input = File.read('inputday4.log').lines.sort
 
 def go_through_log(log, hash={})
   log.each_with_index do |line, index|
@@ -30,12 +30,37 @@ def go_through_log(log, hash={})
   return hash
 end
 
+def part_1
+  parsed = go_through_log(@input)
+  parsed = parsed.reject {|k,v| v.empty?}
+  guard_with_most_sleep = parsed.max_by {|k,v| v.map {|h| h[:delta]}.inject(0) {|sum,x| sum + x }}
+  all_mins = guard_with_most_sleep[1].map {|h| h[:mins_between]}.flatten
+  most_occuring_min = all_mins.max_by { |i| all_mins.count(i) }.to_i
+  guard_num = guard_with_most_sleep[0].split('#').last.to_i
+  pp [guard_num, most_occuring_min, guard_num * most_occuring_min]
+end
 
 
-parsed = go_through_log(input)
-parsed = parsed.reject {|k,v| v.empty?}
-guard_with_most_sleep = parsed.max_by {|k,v| v.map {|h| h[:delta]}.inject(0) {|sum,x| sum + x }}
-all_mins = guard_with_most_sleep[1].map {|h| h[:mins_between]}.flatten
-most_occuring_min = all_mins.max_by { |i| all_mins.count(i) }.to_i
-guard_num = guard_with_most_sleep[0].split('#').last.to_i
-pp [guard_num, most_occuring_min, guard_num * most_occuring_min]
+def part_2
+  p2hash = {}
+  @most_freq_guard, @highest_freq = nil, 0
+  parsed = go_through_log(@input)
+  parsed.each do |guard, sleeps|
+     next if sleeps.size <= 1
+     p2hash[guard] ||= {}
+     sleeps.each do |sleep|
+      sleep[:mins_between].each do |sleep_min|
+        p2hash[guard][sleep_min] ||= 0
+        p2hash[guard][sleep_min] += 1
+        if p2hash[guard][sleep_min] > @highest_freq 
+          @highest_freq = p2hash[guard][sleep_min]
+          @most_freq_guard = guard
+        end
+      end
+    end
+  end
+  g = @most_freq_guard.split(?#)[1].to_i
+  pp [g, @highest_freq, g * @highest_freq] 
+end
+
+ARGV[0] == ?a  ? part_1 : part_2
